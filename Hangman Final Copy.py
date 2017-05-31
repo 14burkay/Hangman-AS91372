e5192caa50d64ee048e75ceca6fcae9392089fa2
14burkay <14burkay@ths.school.nz>
2017-05-31 12:54:46 +1200

    Update Hangman Final Copy.py

--- Hangman Final Copy.py
+++ Hangman Final Copy.py
@@ -1,17 +1,17 @@
 #Kayla Burton
 #20/05/17
 
-import random  #Used to chosen a random word from set list
-#Setting up the modules for hangman     
+import random  #Used to chosen a random word from a set list
 
-def random_word(unjoins_word):
+#Setting up the modules for hangman     
+def random_word(unjoins_word): #This module generates a random word 
     list_of_words = ["skyline", "detonator", "believable", "bridge", "apple", "playground", "daisy", "zombie",
                      "graveyard", "binocular", "mountain", "analyse", "jazz", "abomination", "paralysed", "hallucination",
                      "arrogant", "horizon", "vinyl", "hibernation", "collorbone", "diamond", "bamboo", "echo", "charismatic",
                      "oxygen","pixel", "wave", "wizard", "kayak", "banjo", "kiwifruit", "pneumonia", "puppy", "walkway",
                      "fox"]
     global chosen_word #So chosen_word can be acessed outside of module
-    chosen_word = random.choice(list_of_words) #choses random word
+    chosen_word = random.choice(list_of_words) #choses random word from a set list 
     global guess_word #So guess_word can be acessed outside of module
     guess_word = []#empty list
     for letter in chosen_word:
@@ -19,17 +19,17 @@ def random_word(unjoins_word):
     unjoins_word = " ".join(guess_word) #this prints the _ , as a string as it was a list   
     return unjoins_word
 
-#cheaks is uses guess has a correct input
+#checks if uses guess has a correct input
 def users_guess(already_guessed):
     guess = True
-    while guess != False: #keeps looping until the user inputs a correct input
+    while guess != False: #keeps looping until the user inputs a letter
         global players_guess
         players_guess = input("""
 Please guess a letter """).lower()
-        if len(players_guess) > 1: # check the input is only one letter
+        if len(players_guess) > 1: #checks the input is only one letter
             print("Please try again, your input was more than one letter. ")
                 
-        elif players_guess in already_guessed: # check it letter hasn't been guessed already
+        elif players_guess in already_guessed: #checks if letter hasn't been guessed already
             print("Please try again, you have already guessed that letter.")
             
         elif players_guess not in alphabet: #checks the input is a letter 
@@ -41,18 +41,18 @@ You guessed the letter {}. """.format(players_guess))
             guess = False
             return already_guessed
 
-def display(players_attempts):
+def display(players_attempts): #Displays word and hangman symbols as uses guesses a letter 
     repeat = True
     while repeat != False:
         if "_" in guess_word: #repeats until word has been guessed 
             if players_attempts != 1: #or until attempts have run out
                 users_guess(already_guessed) 
             
-                for letter in range(len(chosen_word)): #loops through chosen word
+                for letter in range(len(chosen_word)): #loops through chosen word and replaces guessed letter if in chosen_word
                     if players_guess == chosen_word[letter]: 
                         guess_word[letter] = players_guess
                 unjoins_word = " ".join(guess_word)
-                print(unjoins_word) #prints the _ version of the random_word but unjoins the _ _
+                print(unjoins_word) #this prints the _ , as a string as it was a list  
                         
 
                 if players_guess not in chosen_word: #players choice isn't in the word
@@ -66,7 +66,7 @@ The letter {} isn't in the word, you now have {} attempts left""".format(players
                 print("Sorry you have zero attempts left, your word was {} ".format(chosen_word))
                 repeat = False#when attempts run out, the game ends
                 if replay == 4:
-                    print("You have played your 3 games. Have a nice day. ") #Will output this when user has players 3 games 
+                    print("You have played your 3 games. Have a nice day. ") #Will output this when user has played 3 games 
     
         else:
             print("Congratulations you guessed the word, {} ".format(chosen_word))
@@ -236,12 +236,12 @@ HANGMAN_SYMBOLS = ["00000000000000000", #shows the ten attempts in picture form
 """]
 
 replay = 1
-while replay != 4: #will repeat the game upto 3 games 
+while replay != 4: #will repeat the game upto 3 times
     play_again = input("Do you want to play hangman? yes/no ").lower()
     if play_again == "yes":
         print("You can play upto 3 games, game {}".format(replay))
         replay += 1 
-        players_attempts = 10#Amount of guesses they user gets 
+        players_attempts = 10#Amount of guesses the user gets 
         already_guessed = []#resets the list when replayed 
         user_name = input("What is your name? ") #Introduction and rules
         print("""Welcome {}, to hangman.
